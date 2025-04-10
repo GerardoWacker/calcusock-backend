982acc4ac4fa443fbc5ea9d14adf7115dcf3a854
Gerardo Wacker <ge@rar.vg>
2025-04-10 13:36:33 -0300

    Agregado un ejemplo de uso a `index.js`
    Para dar mayor contexto sobre cómo utilizar el servidor

--- src/index.js
+++ src/index.js
@@ -19,3 +19,17 @@ socket.start().then(server =>
         console.log('🚀 calcusock-server iniciado con éxito en el puerto ' + port + '.')
     })
 })
+
+// Ejemplo de uso
+//
+// Al crear un cliente, sea en el lenguaje que sea, hay que establecer una conexión con el socket del servidor.
+// Imaginémonos que estamos en un entorno local, deberíamos conectarnos a localhost:1301 (o el puerto personalizado
+// correspondiente.
+//
+// Luego de ello, deberíamos enviar un paquete tradicional, cuyo contenido sea sólamente un string de texto, y que
+// contenga únicamente la operación solicitada. Por ejemplo, enviamos un paquete que diga "(1 + 1) * 2".
+// El servidor lo va a recibir, va a efectuar los parseos y evaluaciones correspondientes, y luego devolverá otro
+// paquete, sin nomenclar, con el resultado. Éste estará dirigido únicamente al cliente que lo envió, debido a que,
+// naturalmente, el paquete `net` ya lo administra de ese modo por defecto.
+//
+// Finalmente, si no hay otra operación que realizar, únicamente se debe cerrar la conexión.
